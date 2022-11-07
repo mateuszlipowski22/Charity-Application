88d898d53a77202008d6660385cf73fae740efce
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-11-07 20:43:43 +0100

    Adding Category Donation and Institution entities

---
+++ src/main/java/pl/coderslab/charity/models/Category.java
@@ -0,0 +1,23 @@
+package pl.coderslab.charity.models;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
