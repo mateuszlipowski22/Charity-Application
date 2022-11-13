bac21746d409894b4c01d8f70904e451a442b8d1
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-11-13 21:15:29 +0100

    Adding jasypt for password hide in allpcation properties

--- src/main/java/pl/coderslab/charity/AppConfig.java
+++ src/main/java/pl/coderslab/charity/AppConfig.java
@@ -1,5 +1,6 @@
 package pl.coderslab.charity;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import pl.coderslab.charity.converters.CategoryDTOConverter;
 import pl.coderslab.charity.converters.InstitutionDTOConverter;
 
 @Configuration
+@EnableEncryptableProperties
 @EnableWebMvc
 @ComponentScan(basePackages = "pl.coderslab")
 
